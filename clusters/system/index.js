0c027de96913deba48518346c123da9cc4a4ee53
Ivan Burnaev <ivan@e-bot7.com>
2020-07-30 13:17:29 +0300

    Forward socket events

--- clusters/system/index.js
+++ clusters/system/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
@@ -6,6 +7,16 @@ app.get('/log', (req, res) => {
   res.json({ status: 'LOGGED' });
 });
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   console.log('SYSTEM is listening at 5000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('System got socket connection');
+
+  socket.on('log', (data, fn) => {
+    fn(`Logy logy log!!! And yeah, here is your data: ${data}`);
+  });
 });
--- index.js
+++ index.js
@@ -1,22 +1,60 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
+const socketClient = require('socket.io-client');
+const socket = require('socket.io');
 
 const app = express();
+app.use(express.static('public'));
+
 const proxy = httpProxy.createProxyServer();
-
-app.use('/iam', function (req, res) {
-  proxy.web(req, res, { target: `http://localhost:4000` });
-});
-
-app.use(`/socket.io`, function (req, res) {
-  proxy.web(req, res, { target: `http://localhost:4000/socket.io` });
-});
-
-// proxy the socket.io WS requests
-app.on('upgrade', function (req, socket, head) {
-  proxy.ws(req, socket, head);
-});
-
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('CORE is listening at 3000');
+});
+
+const clusters = {
+  iam: 'http://localhost:4000',
+  system: 'http://localhost:5000',
+};
+
+const socketEvents = {
+  iam: ['auth'],
+  system: ['log'],
+};
+
+// PROXY WEB
+Object.entries(clusters).forEach(([name, address]) => {
+  app.use(`/${name}`, (req, res) => {
+    proxy.web(req, res, { target: address });
+  });
+});
+
+// CREATE SERVER <=> SERVER SOCKET CONNECTIONS
+const serverSockets = Object.entries(clusters).reduce(
+  (acc, [name, address]) => {
+    acc[name] = socketClient(address);
+    return acc;
+  },
+  {}
+);
+
+// just logs
+Object.entries(serverSockets).forEach(([name, connection]) => {
+  connection.on('connect', () => {
+    console.log(`🖥 connect to the ${name}`);
+  });
+});
+
+// CLIENT SOCKET CONNECTION
+const io = socket(server);
+io.on('connect', (clientSocket) => {
+  console.log('📱 connect to the client');
+
+  // FORWARD CLUSTER SOCKET EVENTS
+  Object.entries(socketEvents).forEach(([name, events]) => {
+    events.forEach((event) => {
+      clientSocket.on(event, (data, fn) => {
+        serverSockets[name].emit(event, data, fn);
+      });
+    });
+  });
 });
